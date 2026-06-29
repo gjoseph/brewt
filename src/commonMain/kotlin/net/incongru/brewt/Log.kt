ce7bed5fd13720183397c2c9bd7ff676173027d3
Greg Joseph <greg.jos@gmail.com>
2026-06-29 14:30:30 +1000

    No need for @ExperimentalTime anymore

--- src/commonMain/kotlin/net/incongru/brewt/Log.kt
+++ src/commonMain/kotlin/net/incongru/brewt/Log.kt
@@ -1,11 +1,9 @@
 package net.incongru.brewt
 
 import kotlin.time.Clock
-import kotlin.time.ExperimentalTime
 
 typealias Logger = (String) -> Unit
 
-@OptIn(ExperimentalTime::class)
 val log: (String) -> Unit = { msg ->
     println("${Clock.System.now()} $msg")
 }
